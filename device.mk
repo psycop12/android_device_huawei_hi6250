864bf9964dff1893778d02a36d01f230054c5823
Meticulus <theonejohnnyd@gmail.com>
2016-10-16 10:32:24 -0500

    TWRP: WIP EXPERIMENTAL fstabSwitcher
    -The idea here is to switch the fstab based on
    what ROM is installed so that this TWRP will work
    properly for both stock and cm. May also need
    to tinker with vold...
    
    -Not tested yet...

--- device.mk
+++ device.mk
@@ -75,8 +75,10 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/rootdir/init.recovery.hisi.rc:root/init.recovery.hisi.rc \
         $(LOCAL_PATH)/recovery/init.recovery.hi6250.rc:root/init.recovery.hi6250.rc \
 	$(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab \
+	$(LOCAL_PATH)/recovery/stock.twrp.fstab:recovery/root/etc/stock.twrp.fstab \
 	$(LOCAL_PATH)/busybox:recovery/root/sbin/busybox \
 	$(LOCAL_PATH)/recovery/bbInstaller:recovery/root/sbin/bbInstaller \
+	$(LOCAL_PATH)/recovery/fstabSwitcher:recovery/root/sbin/fstabSwitcher \
 
 # Media
 PRODUCT_COPY_FILES += \
