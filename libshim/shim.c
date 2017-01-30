a43fa7d58575a7275e8c34b6ddc324bd8c16fda3
Meticulus <theonejohnnyd@gmail.com>
2017-01-30 12:41:39 -0600

    libshim: more symbols for wifi

--- libshim/shim.c
+++ libshim/shim.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
-
+#include <stdlib.h>
+#include <string.h>
 /* liblog */
 extern int __hi_log_print(){ return 0; }
 extern int __android_janklog_print() { return 0; }
@@ -15,4 +16,8 @@ extern void atrace_async_begin_body_HwCam(const char* name,int32_t cookie) {}
 /* libaudio_route */
 extern int pop_seq_init(){return 0;};
 extern int pop_seq_set(){return 0;};
+
+/* wpa_supplicant_hisi */
+extern int __android_log_vprint(int prio, const char *tag, const char *fmt, va_list ap) {return 0;}
+extern int __chr_printLog(){return 0;}
 
