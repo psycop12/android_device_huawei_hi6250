9e3e47ece4ae78a3960e9c09a10034461e0d22a5
Meticulus <theonejohnnyd@gmail.com>
2017-03-13 15:55:08 -0500

    patches: patcher upgrades.
    -Only require critical patches to build.

--- patches/clearpatches.sh
+++ patches/clearpatches.sh
@@ -4,9 +4,21 @@ THISDIR=$PWD
 TOPDIR="$THISDIR/../../../../"
 CLEARED=""
 
+checkerror() {
+    if [[ ${1} -gt 0 ]]; then
+	if [[ "${3}" == "-fatal" ]]; then
+		echo "[$(basename ${0})]: ERROR: ${2}" >&2
+		exit 9
+	else
+		echo "[$(basename ${0})]: WARNING: ${2}" >&2	
+	fi
+    fi
+}
+
+
 clear_set() {
 cd $1
-for LINE in $(echo $(find -name *.patch); echo $(find -name *.apply))
+for LINE in $(echo $(find -L -name *.patch); echo $(find -L -name *.apply); echo $(find -L -name *.disabled); echo $(find -L -name *.disabledapply); echo $(find -L -name *.fixme))
 do
   	REPO=$(dirname $LINE)
 	if [[ $(echo $CLEARED | grep -c "$REPO") -gt 0 ]]; then
@@ -15,7 +27,7 @@ do
 
 	cd $TOPDIR
 	if [[ ! -e $REPO ]]; then
-		echo "WARNING: $REPO does not exist; skipping..."
+		checkerror 1 "$REPO does not exist; skipping..."
 		CLEARED="$CLEARED|$REPO|"
 		cd $THISDIR
 		continue
--- patches/patch.sh
+++ patches/patch.sh
@@ -3,101 +3,98 @@ THISDIR=$PWD
 ROM=${1}
 CLEAR=${2}
 TOPDIR="$THISDIR/../../../../"
+
+
+checkerror() {
+    if [[ ${1} -gt 0 ]]; then
+	if [[ "${3}" == "-fatal" ]]; then
+		echo -e "[$(basename ${0})]: ERROR: ${2}" >&2
+		echo -e "[$(basename ${0})]: \t: #############################################################################" >&2
+		echo -e "[$(basename ${0})]: \t: # ABORT: This ROM may not build or may not boot unless this patch is fixed. #" >&2
+		echo -e "[$(basename ${0})]: \t: #############################################################################" >&2
+		exit 9
+	else
+		echo -e "[$(basename ${0})]: WARNING: ${2}" >&2	
+	fi
+    fi
+}
+
+
 if [[ "$ROM" == "" ]]; then
-	echo "ROM not specified, assuming cm!"
+	cd $TOPDIR
+	if [[ -e vendor/cm ]]; then
+		ROM="cm"
+	elif [[ -e vendor/slim ]]; then
+		ROM="slim"
+	fi
+	echo "ROM not specified. found $ROM"
 	sleep 1
-	ROM="cm"
+	cd $THISDIR	
+	
 fi
 if [[ "$CLEAR" == "" ]]; then
 	./clearpatches.sh $ROM
 fi
 
 cd $ROM
-for LINE in $(find -name *.patch | sort )
+for LINE in $(find -L -name *.patch | sort )
 do
 
 	
 	PATCH=$THISDIR/$ROM/$LINE
 	REPO=$(dirname $LINE)
+	PATCHFATAL=""
 
-	echo "Applying patch: $ROM - $LINE"	
+        if [[ "$(basename $PATCH)" == *critical* ]];then
+	    PATCHFATAL="-fatal"
+	fi
 
 	cd $TOPDIR
 	if [[ ! -e $REPO ]]; then
-		echo "WARNING: $REPO does not exist; skipping..."
+		checkerror 1  "$REPO does not exist; skipping..."
 		cd $THISDIR
 		continue
 	fi
 	cd $REPO
-	RESULT=$(patch -p1 --follow-symlinks --no-backup-if-mismatch < $PATCH)
-	#echo -e "${RESULT}"
-	if [[ $(echo $RESULT | grep -c FAILED) -gt 0 ]] ; then
-		echo ""
-		echo "Fail!"
-		if [[ $UNATTENDED -eq 1 ]]; then
-			exit 9
-		else
-			read -p "Patch Failed!" yn
-			break;
-		fi
+	echo "Applying patch: $ROM - $LINE"
+	patch -p1 --follow-symlinks --no-backup-if-mismatch  < $PATCH &> /dev/null
+	if [[ $? -gt 0 ]]; then
+		checkerror 1 "PATCH FAILED!!! $PATCH" $PATCHFATAL
+		exit 9
 	fi
-	if [[ $(echo $RESULT | grep -c "saving rejects to file") -gt 0 ]] ; then
-		echo ""
-		echo "Fail!"
-		echo "Fix the patch!"
-		if [[ $UNATTENDED -eq 1 ]]; then
-			exit 9
-		else
-			read -p "Patch Rejects!" yn
-			break;
-		fi
-	fi
-	if [[ $(echo $RESULT | grep -c "Skip this patch") -gt 0 ]] ; then
-		echo ""
-		echo "Fail!"
-		echo "Fix the patch!"
-		if [[ $UNATTENDED -eq 1 ]]; then
-			exit 9
-		else
-			read -p "Patch Skipped!" yn
-			break;
-		fi
-	fi
+
 	cd $THISDIR
 done
 
 cd $ROM
-for LINE in $(find -name *.apply | sort )
+for LINE in $(find -L -name *.apply | sort )
 do
 	PATCH=$THISDIR/$ROM/$LINE
 	REPO=$(dirname $LINE)
-	echo "Applying patch: $ROM - $LINE"	
+	PATCHFATAL=""
+
+        if [[ "$(basename $PATCH)" == *critical* ]];then
+	    PATCHFATAL="-fatal"
+	fi
 	
 	cd $TOPDIR
 	if [[ ! -e $REPO ]]; then
-		echo "WARNING: $REPO does not exist; skipping..."
+		checkerror 1 "$REPO does not exist; skipping..."
 		cd $THISDIR
 		continue
 	fi
 	cd $REPO
-	RESULT=$(git apply --whitespace=nowarn -v $PATCH 2>&1)
-	
-	if [[ $(echo $RESULT | grep -c error:) -gt 0 ]] ; then
-		echo ""
-		echo "Fail!"
-		echo "Fix the patch!"
-		if [[ $UNATTENDED -eq 1 ]]; then
-			exit 9
-		else
-			read -p "Patch Error!" yn
-			break;
-		fi
-	fi
+	echo "Applying patch: $ROM - $LINE"
+        git apply --whitespace=nowarn --reject $PATCH &> /dev/null	
+	checkerror $? "APPLY FAILED!!! $PATCH" $PATCHFATAL
 	cd $THISDIR
 done
 cd $THISDIR
 if [[ "$ROM" == "cm" ]];then
 	./patch.sh common false
+elif [[ "$ROM" == "slim" ]]; then
+	./patch.sh common false
 elif [[ "$ROM" == "meticulus" ]]; then
 	./patch.sh cm false
 fi
+
