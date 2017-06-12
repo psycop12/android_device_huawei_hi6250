0c212e3097342187127931a8de027e2401ff432a
Meticulus <theonejohnnyd@gmail.com>
2017-06-12 09:48:55 -0500

    volumeinput: cleanup/simply/license

--- volumeinput/main.c
+++ volumeinput/main.c
@@ -1,61 +1,55 @@
+/*
+ *   Copyright (C) 2017  Jonathan Jason Dennis [Meticulus]
+ *                                       theonejohnnyd@gmail.com
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.#include <linux/input.h>
+ */
+
 #include <linux/input.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/mman.h>
 #include <unistd.h>
 
 #define VOLUMEINPUTPATH "/dev/input/event0"
 
 int main(int argc, char *argv[]) {
-	int fd = -1,retval = -2;
-	fd_set read_set;
-	struct input_event ev;
-	struct timeval tv;
-	int size = sizeof(struct input_event);
-	/* Initialize the structures */
-	memset(&ev, 0x00, sizeof(ev));
-	memset(&tv, 0x00, sizeof(tv));
-
-	/* open input device */
-	if ((fd = open(VOLUMEINPUTPATH, O_RDONLY)) > 0) {
-		while(retval == -2) {
-		/* Intialize the read descriptor */
-		FD_ZERO(&read_set);
-		FD_SET(fd,&read_set);
-		/* Wait up to 0.5 seconds. */
-		tv.tv_sec = 0 ;
-		tv.tv_usec = 500000;
-		if(select(fd+1, &read_set, NULL, NULL, &tv) < 0) {
-			return -1;
-		}
-		/* FD_ISSET(0, &rfds) will be true. */
-		if (FD_ISSET(fd, &read_set)) {
-			read(fd, &ev, size );
-			if(ev.type ==  1) {
-				switch (ev.code) {
-					case 114:
-						printf("down\n");
-						retval = -1;
-						break;
-					case 115:
-						printf("up\n");
-						retval = 0;
-						break;
-
-				}	
-	
-			} 
-	
-		}
-		} 
-	close(fd);
-	} else {
-		printf("Could not open %s", VOLUMEINPUTPATH);
+    int fd = -1,retval = -2;
+    struct input_event ev;
+    int size = sizeof(struct input_event);
+    fd = open(VOLUMEINPUTPATH, O_RDONLY);
+    if (fd) {
+restart:
+	read(fd, &ev, size);
+	if(ev.type ==  1) {
+	    switch (ev.code) {
+		case 114:
+		    printf("down\n");
+		    retval = -1;
+		    break;
+		case 115:
+		    printf("up\n");
+		    retval = 0;
+		    break;
+		default:
+		    goto restart;
+	    }
 	}
-		return retval;	
-
+	close(fd);
+    } else {
+		printf("Could not open %s\n", VOLUMEINPUTPATH);
+    }
+    return retval;
 }
 
 
