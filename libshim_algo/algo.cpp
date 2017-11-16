997b7f92c6e45f6ceeedf1d7f4e398cea0f57e06
Meticulus <theonejohnnyd@gmail.com>
2017-11-16 04:26:01 -0600

    Remove depreciated shim libs

--- libshim_algo/algo.cpp
+++
@@ -1,7 +0,0 @@
-//#include <gui/Sensor.h>
-#include <gui/SensorManager.h>
-
-extern "C" int _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* obj,android::Sensor const* const** list) {
-    android::SensorManager* sm = static_cast<android::SensorManager*>(obj);
-    return sm->getSensorList(list);
-}
