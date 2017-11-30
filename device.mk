a257021fab05791f2a4b8bdfb9aafbd5712ab309
Meticulus <theonejohnnyd@gmail.com>
2017-11-30 05:34:42 -0600

    TWRP v5.1: various changes.
    -Add metiotgd to build
    -Setup meticulus_otg service
    -Remove USB HOST buttons
    -Alphabetize twrp fstab.
    -Add "Universal SafetyNet Fix"

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/rootdir/init.recovery.hisi.rc:root/init.recovery.hisi.rc \
         $(LOCAL_PATH)/recovery/init.recovery.hi6250.rc:root/init.recovery.hi6250.rc \
         $(LOCAL_PATH)/recovery/init.recovery.hi6250.usb.rc:root/init.recovery.hi6250.usb.rc \
+        $(LOCAL_PATH)/recovery/init.recovery.meticulus.rc:root/init.recovery.meticulus.rc \
         $(LOCAL_PATH)/recovery/init.recovery.usb.rc:root/init.recovery.usb.rc
 
 # Media
@@ -72,6 +73,7 @@ PRODUCT_PACKAGES += \
     libcopybit_wrapper \
     lights.hi6250 \
     sensors.hi6250 \
+    metiotgd \
     emui5_vendor_symlinks
 
 # Shim libs
